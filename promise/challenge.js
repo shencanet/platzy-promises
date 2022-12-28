3f8d087d185a612bf467c3eb1e0094d671943d74
shen <santi.dionis@gmail.com>
2022-12-28 20:53:08 +0100

    ADD: promises

--- promise/challenge.js
+++ promise/challenge.js
@@ -1,19 +1,37 @@
 //npm i node-fetch requisitos 
 
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1';
 
-function fetchData(urlAPI){
-    return fetch(urlAPI);
-};
-
 //función que va a recibir como argumento la url que queremos llamar y esto retornará el llamado de fecth: una promesa
-fetchData(`$(API)/products`)
+function fetchData(urlApi){
+    return fetch(urlApi);
+};
+/*
+fetchData(`${API}/products`)
     .then(response => response.json())
     .then(products => {
         console.log(products);
-    }) 
-    .then(() => {//se pueden anidar múltiples .then
-        console.log('hola')
     })
-    .catch(error => console.log(error));
+    .then(() => {
+        console.log('hola');
+    }) //se pueden anidar múltiples .then
+    .catch(error => console.log(error));*/
+
+    fetchData(`${API}/products`)
+    .then(response => response.json()) //se hace la conversión a un objeto json
+    .then(products => {
+        console.log(products);
+        return fetchData(`${API}/products/${products[0].id}`) // solo se quiere mostrar el primer elemento de la primera solicitud
+    })
+    .then(response => response.json()) //se vuelve traer la data
+    .then(product => {
+        console.log(products.title);
+        return fetchData(`${API}/categories/${product.category.id}`) //se quiere mostrar la categoria de un producto en particular
+    })
+    .then(response => response.json())
+    .then(category => {
+        console.log(category.name);
+    })
+    .catch(err => console.log(err)) //detectar un error
+    .finally(() => console.log('Finally')); //es opcional para mostrar que se terminó la solicitud
